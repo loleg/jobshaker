3a458cd1fca8d16dd6c62f175834b5b8ca5d6d76
Oleg Lavrovsky <oleg@utou.ch>
2012-02-10 00:02:16 +0100

    Model redesign

--- posts/admin.py
+++ posts/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 admin.site.register(UserProfile)
 admin.site.register(Profession)
-admin.site.register(Place)
+admin.site.register(Intent)
 admin.site.register(Post)
 admin.site.register(Reply)
--- posts/models.py
+++ posts/models.py
@@ -2,32 +2,44 @@ from django.db import models
 from django.contrib import auth
 import datetime
 
-class Profession(models.Model):
-	name = models.CharField(max_length=30)
+class Intent(models.Model):
+	name = models.CharField(max_length=70)
+	icon = models.CharField(max_length=99)
+	back = models.CharField(max_length=99)
 	def __unicode__(self):
 		return self.name
-	
-class Place(models.Model):
-	name = models.CharField(max_length=30)
+
+class Profession(models.Model):
+	name = models.CharField(max_length=50)
 	def __unicode__(self):
 		return self.name
 	
 class UserProfile(models.Model):
 	user = models.OneToOneField(auth.models.User)
 	nickname = models.CharField(max_length=30)
-	location = models.ForeignKey(Place)
 	profession = models.ForeignKey(Profession)
+	postcode = models.IntegerField()
+	english = models.BooleanField()
+	german = models.BooleanField()
+	french = models.BooleanField()
+	italian = models.BooleanField()
+	other_lang = models.CharField('other languages', max_length=70)
+	age = models.IntegerField()
+	sex = models.CharField(max_length=1)
 	def __unicode__(self):
 		return self.nickname
 
 class Post(models.Model):
 	pub_date = models.DateTimeField('date published', auto_now_add=True)
 	user = models.ForeignKey(UserProfile)
-	place = models.ForeignKey(Place)
-	intent = models.CharField(max_length=70)
+	postcode = models.IntegerField()
+	distance = models.IntegerField()
+	intent = models.ForeignKey(Intent)
+	valid_from = models.DateField('from') 
+	valid_until = models.DateField('until') 
 	comment = models.TextField()
 	def __unicode__(self):
-		return self.intent
+		return self.intent.name
 	def was_published_today(self):
 		return self.pub_date.date() == datetime.date.today()
 	
