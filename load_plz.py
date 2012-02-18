7904e529769310f0e306f4b9645dc7b57a7f5e77
Oleg Lavrovsky <oleg@utou.ch>
2012-02-18 18:21:32 +0100

    Added data fixtures

--- load_plz.py
+++ load_plz.py
@@ -6,11 +6,14 @@ setup_environ(settings)
 from information.models import Location
 
 import csv
+import codecs
+
+dataFormat = 'mbcs';
 
 class LoadData():
 	print "Loading location data"
 	count = 0
-	with open('db/plz_l_20120124.txt', 'rb') as f:
+	with codecs.open('db/plz_l_20120124.txt', 'rb', dataFormat) as f:
 		reader = csv.reader(f, dialect=csv.excel_tab)
 		for row in reader:
 			print row[2], '=', row[4]
--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,6 @@ from posts.models import *
 from django.contrib import admin
 
 admin.site.register(UserProfile)
-admin.site.register(Profession)
 admin.site.register(Intent)
 admin.site.register(Post)
 admin.site.register(Reply)
