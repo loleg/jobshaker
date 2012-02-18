5aa2f84ba87c24052c1af376bcd511b3a8d34884
Oleg Lavrovsky <oleg@utou.ch>
2012-02-18 18:49:04 +0100

    Updated PLZ loader

--- load_plz.py
+++ load_plz.py
@@ -8,12 +8,12 @@ from information.models import Location
 import csv
 import codecs
 
-dataFormat = 'mbcs';
+dataFormat = 'utf-8' #'iso-8859-15'
 
 class LoadData():
 	print "Loading location data"
 	count = 0
-	with codecs.open('db/plz_l_20120124.txt', 'rb', dataFormat) as f:
+	with codecs.open('db/plz_l_20120124.txt', 'rb') as f:
 		reader = csv.reader(f, dialect=csv.excel_tab)
 		for row in reader:
 			print row[2], '=', row[4]
