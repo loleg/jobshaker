ebd2f096b84121ecddbbf809d0a3a9016119f609
Oleg Lavrovsky <oleg@utou.ch>
2012-02-21 21:56:53 +0100

    Nav class, post template

--- posts/static/main.js
+++ posts/static/main.js
@@ -2,4 +2,13 @@ $(function() {
 	$(".datePicker").datepicker();
 	$(".button").button();
 	$(".btnCancel").click(function() { window.history.back(); });
+	
+	switch (document.location.pathname) {
+	case '/':
+		$('.menu .home').addClass('current'); break;
+	case '/userprofile/':
+		$('.menu .profile').addClass('current'); break;
+	case '/userprofile/replies/':
+		$('.menu .posts').addClass('current'); break;
+	}
 });
